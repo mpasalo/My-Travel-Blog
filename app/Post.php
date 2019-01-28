21b673fa1fcdf69fe162b83c25bff0e2eb3316fb
Marc Paul Pasalo <pasalomarc@gmail.com>
2019-01-28 16:39:14 +0800

    minor changes v2.1

--- app/Post.php
+++ app/Post.php
@@ -33,22 +33,5 @@ class Post extends Model
 
 	}
 
-	// public function scopeFilter($query, $filters)
 
-	// {
-
-	// 	if ($month = $filters['month']) {
-
-	// 	   $query->whereMonth('created_at', Carbon::parse($month)->month);
-
-	// 	}
-
-
-	// 	if ($year = $filters['year']) {
-
-	// 	   $query->whereYear('created_at', $year);
-
-	// 	}
-
-	// }
 }  
