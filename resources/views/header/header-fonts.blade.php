0a0e50ceb02c891164899fdffa42f415d308d30e
Marc Paul <pasalomarc@gmail.com>
2020-09-24 22:23:29 +0800

    divide master blade and use include

---
+++ resources/views/header/header-fonts.blade.php
@@ -0,0 +1,4 @@
+<link href="https://fonts.googleapis.com/css?family=Playfair+Display:700,900" rel="stylesheet">
+<link href='https://fonts.googleapis.com/css?family=Homemade Apple' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Architects Daughter' rel='stylesheet'>
+<link href='https://fonts.googleapis.com/css?family=Nothing You Could Do' rel='stylesheet'>
---
+++ resources/views/header/header.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+
+    <head>
+        <title>My Travel Blog</title>
+
+        @include('header.header-meta')
+
+        @include('header.header-css')
+
+        @include('header.header-fonts')
+
+        @include('header.header-js')
+    </head>
