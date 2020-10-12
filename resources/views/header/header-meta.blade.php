5802a4a93b69511a689e2c8734bdf5b67dbb6361
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-12 11:30:45 +0800

    add csrf meta

--- resources/views/header/header-meta.blade.php
+++ resources/views/header/header-meta.blade.php
@@ -3,3 +3,4 @@
 <meta name="description" content="">
 <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
 <meta name="generator" content="Jekyll v3.8.5">
+<meta name="csrf-token" content="{{ csrf_token() }}">
