d8159b910266f3223295a182357921a7953a67fd
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-21 16:19:04 +0800

    add delete button

--- resources/js/components/posts/PostComponent.vue
+++ resources/js/components/posts/PostComponent.vue
@@ -1,10 +1,18 @@
 <template>
     <div class="blog-post">
-        <h2 class="blog-post-title">
-            <a :href="`/posts/${post.id}`">
-                {{ post.title }}
-            </a>
-        </h2>
+        <div class="d-flex flex-row align-items-center">
+            <h2 class="blog-post-title">
+                <a :href="`/posts/${post.id}`">
+                    {{ post.title }}
+                </a>
+            </h2>
+
+            <ion-icon
+                name="trash-outline"
+                class="btn btn-danger btn-sm m-4"
+                @click="deletePost()"
+            ></ion-icon>
+        </div>
         <p class="blog-post-meta">
             {{ post.user.name }} on
 
@@ -15,8 +23,14 @@
 </template>
 
 <script>
-    export default {
-        name: 'PostComponent',
-        props: ['post'],
-    };
+export default {
+    name: "PostComponent",
+    props: ["post"],
+
+    methods: {
+        deletePost: function() {
+            this.$store.dispatch("post/delete", this.post.id);
+        }
+    }
+};
 </script>
