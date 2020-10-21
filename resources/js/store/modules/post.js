a626d97387a05e12bbb1e9d56552e1489650900f
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-21 11:57:30 +0800

    add error alerts

--- resources/js/store/modules/post.js
+++ resources/js/store/modules/post.js
@@ -1,5 +1,34 @@
 export const actions = {
     create(context, data) {
-        axios.post('/posts', data.formData);
-    },
-}
+        axios.post("/posts", data.formData).then(response => {
+            if (response.data.message) {
+                var arr = [].concat.apply(
+                    [],
+                    [response.data.message.title, response.data.message.body]
+                );
+                let error_fields = arr.filter(function(e) {
+                    if (e) {
+                        return e;
+                    } else {
+                        return null;
+                    }
+                });
+                Vue.swal({
+                    title: "Blog Post",
+                    html: error_fields,
+                    icon: "error",
+                    confirmButtonText: "Ok"
+                });
+            } else {
+                Vue.swal({
+                    title: "Blog Post",
+                    html: "Post Created Succesfully",
+                    type: "success",
+                    confirmButtonText: "Ok"
+                }).then(response => {
+                    location.assign("/posts");
+                });
+            }
+        });
+    }
+};
