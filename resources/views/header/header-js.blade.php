283c7e197c7b850bc3262fc2d5f27b4a5aa29496
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-21 16:20:45 +0800

    add ionicons

--- resources/views/header/header-js.blade.php
+++ resources/views/header/header-js.blade.php
@@ -1,1 +1,3 @@
 <script src="{{ asset('js/app.js') }}" defer></script>
+<script type="module" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule="" src="https://unpkg.com/ionicons@5.2.3/dist/ionicons/ionicons.js"></script>
