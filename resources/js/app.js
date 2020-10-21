c2e31167c9a0712275eb28cff477cf63619035c0
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-21 16:21:25 +0800

    format app.js and add config for ion

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,17 +1,17 @@
-
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
+require("./bootstrap");
 
-window.Vue = require('vue');
+window.Vue = require("vue");
 
-import store from './store';
-import VueSweetalert2 from 'vue-sweetalert2';
+import store from "./store";
+import VueSweetalert2 from "vue-sweetalert2";
 
+Vue.config.ignoredElements = [/^ion-/];
 Vue.use(VueSweetalert2);
 
 /**
@@ -25,11 +25,26 @@ Vue.use(VueSweetalert2);
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-Vue.component('index-post-component', require('./components/posts/IndexPostComponent.vue').default);
-Vue.component('post-component', require('./components/posts/PostComponent.vue').default);
-Vue.component('create-post-component', require('./components/posts/CreatePostComponent.vue').default);
-Vue.component('show-post-component', require('./components/posts/ShowPostComponent.vue').default);
+Vue.component(
+    "example-component",
+    require("./components/ExampleComponent.vue").default
+);
+Vue.component(
+    "index-post-component",
+    require("./components/posts/IndexPostComponent.vue").default
+);
+Vue.component(
+    "post-component",
+    require("./components/posts/PostComponent.vue").default
+);
+Vue.component(
+    "create-post-component",
+    require("./components/posts/CreatePostComponent.vue").default
+);
+Vue.component(
+    "show-post-component",
+    require("./components/posts/ShowPostComponent.vue").default
+);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
@@ -38,6 +53,6 @@ Vue.component('show-post-component', require('./components/posts/ShowPostCompone
  */
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     store
 });
