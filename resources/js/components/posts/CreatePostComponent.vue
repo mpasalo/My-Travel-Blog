8783c7baac73b47b83ca20b08efc5dd7ea574c6f
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-21 11:57:11 +0800

    fix format and move alert

--- resources/js/components/posts/CreatePostComponent.vue
+++ resources/js/components/posts/CreatePostComponent.vue
@@ -1,69 +1,64 @@
 <template>
     <div class="col-md-8 blog-main">
         <h1 class="title">Create A New Post</h1>
-            <div class="form-group">
-                <label class="label" for="title">Post Title</label>
-                <input type="text" 
-                    class="input form-control" 
-                    name="title"
-                    placeholder="Post Title" 
-                    v-model="form.title">
+        <div class="form-group">
+            <label class="label" for="title">Post Title</label>
+            <input
+                type="text"
+                class="input form-control"
+                name="title"
+                placeholder="Post Title"
+                v-model="form.title"
+            />
+        </div>
+        <div class="form-group">
+            <label class="label" for="body">Blog Content</label>
+            <textarea
+                name="body"
+                class="textarea form-control"
+                placeholder="Blog Content"
+                v-model="form.body"
+            ></textarea>
+        </div>
+        <div class="form-group">
+            <div class="control">
+                <button @click="saveBlogPost()" class="btn btn-danger">
+                    Publish Post
+                </button>
             </div>
-            <div class="form-group">
-                <label class="label" for="body">Blog Content</label>
-                <textarea 
-                    name="body" 
-                    class="textarea form-control"
-                    placeholder="Blog Content" 
-                    v-model="form.body"></textarea>
-            </div>
-            <div class="form-group">
-                <div class="control">
-                    <button @click="saveBlogPost()" class="btn btn-danger">Publish Post</button>
-                </div>  
-            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    import Form from 'vform';
-    import objectToFormData from 'object-to-formdata';
+import Form from "vform";
+import objectToFormData from "object-to-formdata";
 
-    window.objectToFormData = objectToFormData
-    export default {
-        name: 'CreatePostComponent',
-        data() {
-            return {
-                form          : new Form({
-                    title     : '',
-                    body      : '',
-                }),
+window.objectToFormData = objectToFormData;
+export default {
+    name: "CreatePostComponent",
+    data() {
+        return {
+            form: new Form({
+                title: "",
+                body: ""
+            })
+        };
+    },
+
+    methods: {
+        saveBlogPost: function() {
+            let formData = new FormData();
+
+            formData.append("title", this.form.title);
+            formData.append("body", this.form.body);
+
+            let data = {
+                formData: formData
             };
-        },
 
-        methods: {
-            saveBlogPost: function() {
-                let formData = new FormData();
-
-                formData.append('title', this.form.title);
-                formData.append('body', this.form.body);
-
-                let data = {
-                    formData: formData,
-                };
-
-                this.$store.dispatch('post/create', data)
-                    .then((response) => {
-                        this.$swal({
-                            title: 'Blog Post',
-                            html: 'Post Created Succesfully',
-                            type: 'success',
-                            confirmButtonText: "Ok",
-                        }).then((response) => {
-                            location.assign('/posts');  
-                        });
-                    });
-            },
+            this.$store.dispatch("post/create", data);
         }
-    };
+    }
+};
 </script>
