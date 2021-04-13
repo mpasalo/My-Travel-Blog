96ba409d9fcda07bf854b825972193fec4d0b78e
Marc_Pasalo <marc.paulpasalo@mymoovz.com>
2021-04-13 10:59:27 +0800

    remove unused and add GitHub link

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,28 +1,12 @@
 <aside class="col-md-4 blog-sidebar">
     <div class="p-3 mb-3 bg-light rounded">
         <h4 class="font-italic">About</h4>
-        <p class="mb-0">Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
-    </div>
-    <div class="p-3">
-        <h4 class="font-italic">Archives</h4>
-        <ol class="list-unstyled mb-0">
-            @if (isset($archives))
-                @foreach ($archives as $month)
-                    <li>
-                        <a href="/posts/?month={{ $month['month'] }}&year={{ $month['year'] }}">
-                            {{ $month['month'] .' '. $month['year'] }}
-                        </a>
-                    </li>
-                @endforeach
-            @endif
-        </ol>
+        <p class="mb-0">A Travel Blog made by Marc Pasalo</p>
     </div>
     <div class="p-3">
         <h4 class="font-italic">Elsewhere</h4>
         <ol class="list-unstyled">
-            <li><a href="#">GitHub</a></li>
-            <li><a href="#">Twitter</a></li>
-            <li><a href="#">Facebook</a></li>
+            <li><a href="https://github.com/mpasalo/My-Travel-Blog" target="_blank">GitHub</a></li>
         </ol>
     </div>
 </aside>
